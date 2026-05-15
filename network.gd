ad7f2742aee6c1fc80b0938ed9cc7fcd512d23b7
etper <piotr.brzezinski9@gmail.com>
2026-05-15 09:12:13 +0200

    agaaaaaain

--- network.gd
+++ network.gd
@@ -1,45 +1,57 @@
 extends Node
 
 const PORT = 9999
+
 @export var player_scene: PackedScene
 
 var auto_host = false
 var join_ip = ""
 
-func host_game():
-	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(PORT)
-	multiplayer.multiplayer_peer = peer
-
-	print("Hosting game")
-
-	_spawn_player(multiplayer.get_unique_id())
-
-func join_game(ip):
-	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(ip, PORT)
-	multiplayer.multiplayer_peer = peer
-
-	print("Joining game")
-
 func _ready():
-	multiplayer.peer_connected.connect(_player_connected)
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	multiplayer.connected_to_server.connect(_connected_ok)
 
 	if auto_host:
 		host_game()
-
 	elif join_ip != "":
 		join_game(join_ip)
 
-func _player_connected(id):
-	if multiplayer.is_server():
-		_spawn_player(id)
+func host_game():
+	var peer = ENetMultiplayerPeer.new()
 
+	peer.create_server(PORT)
+	multiplayer.multiplayer_peer = peer
+
+	print("Hosting")
+
+	_spawn_player.rpc(multiplayer.get_unique_id())
+
+func join_game(ip):
+	var peer = ENetMultiplayerPeer.new()
+
+	peer.create_client(ip, PORT)
+	multiplayer.multiplayer_peer = peer
+
+	print("Joining")
+
+func _connected_ok():
+	print("Connected to server")
+
+func _on_peer_connected(id):
+	print("Peer connected: ", id)
+
+	if multiplayer.is_server():
+		_spawn_player.rpc(id)
+
+@rpc("authority", "call_local")
 func _spawn_player(id):
+	if has_node(str(id)):
+		return
+
 	var player = player_scene.instantiate()
 
-	add_child(player)
-
 	player.name = str(id)
+
+	add_child(player)
 
 	player.set_multiplayer_authority(id)
